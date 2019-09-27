1b0706806d4a3c12e538e0de86a55e61761a600f
Alan Curtis <acurtis@badgersport.local>
2019-09-27 17:22:07 -0600

    remove old input component

--- src/views/AView01/AView01.js
+++ src/views/AView01/AView01.js
@@ -4,13 +4,11 @@ import ATestRedux from '../../componentsExamples/ATestRedux/ATestRedux';
 import ATestRouting from '../../componentsExamples/ATestRouting/ATestRouting';
 import ABasicComp from '../../componentsExamples/ABasicComp/ABasicComp';
 import ABasicStatelessComp from '../../componentsExamples/ABasicStatelessComp/ABasicStatelessComp';
-import WView01Input from '../../componentsExamples/wrappers/WView01Input/WView01Input';
 
 function AView01 (props) {
   return (
     <div className="AView01">
        AView01 Comp
-       <WView01Input />
        <ABasicStatelessComp />
        <ABasicComp />
        <ATestButtonAxios />
